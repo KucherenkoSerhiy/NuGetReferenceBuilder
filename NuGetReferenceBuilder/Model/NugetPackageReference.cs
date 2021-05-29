f7ffb0d15d258eb3a62f713eb05872d9c36f534d
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-05-29 05:14:19 +0200

    created Parameters and NugetPackageReference models

---
+++ NuGetReferenceBuilder/Model/NugetPackageReference.cs
@@ -0,0 +1,9 @@
+﻿namespace NuGetReferenceBuilder.Model
+{
+    public class NugetPackageReference
+    {
+        public string Name { get; set; }
+        public string CurrentVersion { get; set; }
+        public string TargetVersion { get; set; }
+    }
+}
---
+++ NuGetReferenceBuilder/Model/Parameters.cs
@@ -0,0 +1,15 @@
+﻿namespace NuGetReferenceBuilder.Model
+{
+    public class Parameters
+    {
+        public string RepositoriesType { get; set; }
+        public string RootPath { get; set; }
+
+        public string MasterBranchName { get; set; }
+        public string FeatureBranchName { get; set; }
+        public bool WillCommitChanges { get; set; }
+        public bool WillPushChanges { get; set; }
+        public bool WillUpdateSubmodules { get; set; }
+        public bool WillCreatePullRequest { get; set; }
+    }
+}
